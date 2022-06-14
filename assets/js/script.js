c2e49c5b764364e025f213a7f24f57b25aadfd41
Brandon Franks <captainxcuddles@hotmail.com>
2022-06-13 19:44:13 -0600

    JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,42 @@ var dateTime = function() {
 }
 
 // text box needs to change color to match past current or future tense
+function textColor() {
+    var timeColor = moment().hours();
+    $('.time-block').each(function(){
+        var now = parseInt($(this).attr("id"));
+        if(now < timeColor) {
+            $(this).addClass("past");
+        }
+        else if (now === timeColor) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("future");
+        }
+    })
+};
+
+var saveBtn = document.querySelector(".saveBtn")
+
+function saveText() {
+    var timeTask = $(this).siblings(".hour").text();
+    var tasks = $(this).siblings(".tasks").val();
+    console.log($(this));
+
+    localStorage.setItem(timeTask, tasks);
+};
+
+// when reloading the page content and info is pulled from storage
+function clickHandler() {
+    var data = $(this).siblings("textarea");
+    var userInput = data.val();
+    var id = data.attr("id");
+    localStorage.setItem(id, userInput);
+  };
+  $(".saveBtn").click(clickHandler)
+
+// event listeners
+saveBtn.addEventListener("click", saveText)
+
+textColor();
